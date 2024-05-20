4d94bd8a699e8ef604cc4e2930d225e87c67cec0
namraravani <namraravani8@gmail.com>
2024-05-20 13:59:30 +0530

    Service_fix_commit

--- config/services.php
+++ config/services.php
@@ -29,4 +29,16 @@ return [
     'secret' => env('AWS_SECRET_ACCESS_KEY'),
     'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
   ],
+
+  'google' => [
+    'client_id' => env('GOOGLE_CLIENT_ID'),
+    'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+    'redirect' => env('GOOGLE_REDIRECT_URI'),
+  ],
+
+  'facebook' => [
+    'client_id' => '824649775838850',
+    'client_secret' => '402c95754980c7bcb3e0ab0872111f5f',
+    'redirect' => 'http://localhost:8000/facebook/callback',
+  ],
 ];
